6a4f510961d099c26064ea0c36aef288ca5307a6
JoeZhu123 <1668045443@qq.com>
2016-07-30 10:32:43 +0800

    C++ program by using OpenCV to detect face/eyes..

---
+++ mm_face/CMakeFiles/mm_facedetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mm_facedetect.dir/mm_facedetect.cpp.o"
+  "mm_facedetect.pdb"
+  "mm_facedetect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mm_facedetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
